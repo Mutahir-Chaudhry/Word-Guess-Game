9a98b51a1b971acf5b1373aa6b63863a3dd38579
Mutahir-Chaudhry <mutachaudhry@gmail.com>
2019-06-18 17:14:59 -0400

    Temp working incomplete game.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,16 +2,16 @@
     var colors = ["red", "blue", "green", "yellow", "black", "white", "orange", "purple"];
     
     //Creating variable to store the word to be used from the colors array. Using the Math.floor(Math.random) forula and set to run through the length of the entire array. 
-    var word = words[Math.floor(Math.random()*colors.length)];
+    var word = colors[Math.floor(Math.random()*colors.length)];
 
     //Creating an array to capture the letters input by the user. Creating an empty array, we set a for loop within it to create as many _ as the length of the word chosen. 
     var arrayAnswer = [];
-    for (var i = 0; i < colors.length; i++) {
-        arrayAnswer[i] = "_";
+    for (var a = 0; a < word.length; a++) {
+        arrayAnswer[a] = "_";
     }
 
     //Creating variable to keep track of the number of letters left to guess for the color chosen. It is set to the lengnth of the number of letters in the color chosen. 
-    var remainingLetters = colors.length;
+    var remainingLetters = word.length;
 
     //Beginning the while loop required for the game, set to run as long as there are missing letters left to be guessed in the word. 
     while (remainingLetters > 0) {
@@ -27,12 +27,18 @@
         if (guess === null) {
             break;
         } 
-        //This next part is particular to alerts, since we arent going off of sinlge key strokes just yet. For now, we want the basic game itself to work, whether we are displaying on the page or in alerts. Hence, this else if statement creates an alert if the user inputs anything longer than 1 letter. 
+        //This next part is particular to alerts, since we arent going off of sinlge key strokes just yet. For now, we want the basic game itself to work, whether we are displaying on the page or in alerts. Hence, this else if statement creates an alert if the user inputs anything other than a single letter. 
         else if (guess.length !== 1) {
             alert("Please enter just ONE letter!");
         }
         //
-        else {
+        else { 
+            for (var b = 0; b < word.length; b++){
+            if (word[b] === guess) {
+                arrayAnswer[b] = guess;
+                remainingLetters--;
+            }
+        }
             //We will update the game state with the users input
         }   
         //Game Code
