06a6928b10ffaf1f4c77624704ca0765d2e27475
Mutahir-Chaudhry <mutachaudhry@gmail.com>
2019-06-18 19:30:45 -0400

    added submission function with for loop an the necessary if and else statements

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,38 +4,25 @@
     var randomizer = [Math.floor(Math.random()*colors.length)];
     //Creating array to hold the actual color chosen, storing it in the variable word. 
     var word = colors[randomizer];
-    // //
-    // console.log("word");
-    // console.log(word);
-    
     //Creating variable to store user length
     var userLength = word.length
-
     //Creating an array for the user display. 
     var userDisplay = [userLength]
-
     //Creating variable for win
     var win = userLength;
-
     //creating variable for the letters within the color chosen
     var letters = word.split('');
-
     //Creating variable to store the number of attempts remaining
     var attemptsLeft = 10;
-
     //Creating variable to combine and present display 
     var output = "";
-
     //Creating varaible to store the users letter
     var userLetter = "";
-
     //Creating variable to display wins and losses
     var wins = 0;
     var loses = 0;
-
     //varaible for the initial function
     var initial = function (){
-    
     //for loop within it to create as many _ as the length of the word chosen using the userDisplay array
     for (var a = 0; a < randomizer.length; a++) {
         userDisplay [a] = "_";
@@ -44,47 +31,71 @@
     document.getElementById("colors").innerHTML=output;
     output="";
 }
-
-    var submission = funtion (){
+    //function for letter submitted
+    var submission = function (){
         output = "";
         userLetter = $("letter").value;
         $("letter").value = "";
 
-        for (var b = 0; b < word.length)
-    }
-    // // //Creating variable to keep track of the number of letters left to guess for the color chosen. It is set to the lengnth of the number of letters in the color chosen. 
-    // var remainingLetters = word.length;
-    //Beginning the while loop required for the game, set to run as long as there are missing letters left to be guessed in the word. 
-    while (userLength > 0) {
-        //Creating a document.text to display player progress.
-        alert(userDisplay.join(" "));
-        //Input from Player code
-        //Creating variable to store the guess from the player, inputed into the prompt created. 
-        var guess = prompt("Guess a letter");
-        //Creating if else statement for the possibilities of user input taken
-        //First if statement is if the user cancels, making their guess null. 
-        if (guess === null) {
-            break;
-        } 
-        //This next part is particular to alerts, since we arent going off of sinlge key strokes just yet. For now, we want the basic game itself to work, whether we are displaying on the page or in alerts. Hence, this else if statement creates an alert if the user inputs anything other than a single letter. 
-        else if (guess.length !== 1) {
-            alert("Please enter just ONE letter!");
-        }
-        //
-        else { 
-            for (var b = 0; b < randomizer.length; b++){
-            if (randomizer[b] === guess) {
-                userDisplay[b] = guess;
-                userLength--;
+        for (var b = 0; b < word.length; b++){
+            alert (letters[b]);
+            if (userLetter.toLowerCase()==letters[b]){
+                userDisplay[b] = userLetter.toLowerCase();
+                win--;
             }
+            output = output + display[b] + "";
         }
-            //We will update the game state with the users input
-        }   
-        //Game Code
-        //Player Progress
-        //Input from Player
-        //Update both previously created array and remaingLetters everytime user guesses correctly.
+        document.getElementById("colors").innerHTML=output;
+        output="";
+        attemptsLeft--;
+        if (win > 1)
+        {
+            document.getElementById("guesses").innerHTML = "You Win";
+        }
+        else if (attemptsLeft < 1)
+        {
+            document.getElementById("guesses").innerHTML = "You Lose";
+        }
+        else
+        {
+            document.getElementById("guesses").innerHTML = "You have " + attemptsLeft + "guesses left";
+        }
     }
+
+    
+    // // // //Creating variable to keep track of the number of letters left to guess for the color chosen. It is set to the lengnth of the number of letters in the color chosen. 
+    // // var remainingLetters = word.length;
+    // //Beginning the while loop required for the game, set to run as long as there are missing letters left to be guessed in the word. 
+    // while (userLength > 0) {
+    //     //Creating a document.text to display player progress.
+    //     alert(userDisplay.join(" "));
+    //     //Input from Player code
+    //     //Creating variable to store the guess from the player, inputed into the prompt created. 
+    //     var guess = prompt("Guess a letter");
+    //     //Creating if else statement for the possibilities of user input taken
+    //     //First if statement is if the user cancels, making their guess null. 
+    //     if (guess === null) {
+    //         break;
+    //     } 
+    //     //This next part is particular to alerts, since we arent going off of sinlge key strokes just yet. For now, we want the basic game itself to work, whether we are displaying on the page or in alerts. Hence, this else if statement creates an alert if the user inputs anything other than a single letter. 
+    //     else if (guess.length !== 1) {
+    //         alert("Please enter just ONE letter!");
+    //     }
+    //     //
+    //     else { 
+    //         for (var b = 0; b < randomizer.length; b++){
+    //         if (randomizer[b] === guess) {
+    //             userDisplay[b] = guess;
+    //             userLength--;
+    //         }
+    //     }
+    //         //We will update the game state with the users input
+    //     }   
+    //     //Game Code
+    //     //Player Progress
+    //     //Input from Player
+    //     //Update both previously created array and remaingLetters everytime user guesses correctly.
+    // }
 
 
 
